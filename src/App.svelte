8d6697deb2aaa64dc3e412ebf91ad8701df96bf1
MCTVR <69135058+MCTVR@users.noreply.github.com>
2022-05-14 15:30:53 +0800

    Initial Start

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+	import { writable } from 'svelte/store';
+	import SearchComp from './SearchComp.svelte';
+	import Clock from './Clock.svelte';
+
+</script>
+
+<div id="app">
+	<bg></bg>
+	<Clock />
+	<SearchComp />
+</div>
+
+<style>
+	:global(body) {
+		overflow: hidden;
+		margin: 0;
+		padding: 0;
+	}
+
+	#app {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		height: 100vh;
+		width: 100vw;
+		background-color: transparent;
+	}
+
+	:global(bg) {
+		position: fixed;
+		z-index: -1;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-image: url('/icons/HK.webp');
+		background-repeat: no-repeat;
+		background-size: cover;
+		background-position: center;
+		background-attachment: fixed;
+	}
+
+</style>
