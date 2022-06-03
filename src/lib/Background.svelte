ee21efaed343841f70f6df2dddfa5fffb8c00ddf
MCTVR <69135058+MCTVR@users.noreply.github.com>
2022-06-03 12:39:40 +0800

    .

--- src/lib/Background.svelte
+++ src/lib/Background.svelte
@@ -1,26 +1,23 @@
 <script>
-
-    export let imgURL = "icons/HK.webp";
-
+	export let imgURL = "./icons/HK.webp";
 </script>
 
 <div id="bg">
-    <img src={imgURL} alt="" />
+	<img src={imgURL} alt="" />
 </div>
 
-
 <style>
-    #bg {
-        position: fixed;
+	#bg {
+		position: fixed;
 		z-index: -1;
 		top: 0;
 		left: 0;
 		width: 100%;
 		height: 100%;
-    }
-    #bg img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
+	}
+	#bg img {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
 </style>
--- src/lib/Clock.svelte
+++ src/lib/Clock.svelte
@@ -1,50 +1,49 @@
 <script>
-    let time;
+	let time;
 
-    const getTimezone = () => {
-        const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-        return timezone;
-    };
+	const getTimezone = () => {
+		const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+		return timezone;
+	};
 
-    const getLocale = () => {
-        const locale = Intl.DateTimeFormat().resolvedOptions().locale;
-        return locale;
-    }
+	const getLocale = () => {
+		const locale = Intl.DateTimeFormat().resolvedOptions().locale;
+		return locale;
+	};
 
-    const getTime = () => {
-        const nowTime = new Date().toLocaleTimeString(getLocale(), {
-            timeZone: getTimezone(),
-            hour12: false,
-            hour: '2-digit',
-            minute: '2-digit'
-        });
-        time = nowTime;
-        setTimeout(getTime, 1000);
-    }
+	const getTime = () => {
+		const nowTime = new Date().toLocaleTimeString(getLocale(), {
+			timeZone: getTimezone(),
+			hour12: false,
+			hour: "2-digit",
+			minute: "2-digit",
+		});
+		time = nowTime;
+		setTimeout(getTime, 1000);
+	};
 
-    getTime();
-
+	getTime();
 </script>
 
 <div id="clock">
-    <h1 id="clock-digits">{time}</h1>
+	<h1 id="clock-digits">{time}</h1>
 </div>
 
 <style>
-    #clock {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        width: 100vw;
-        user-select: none;
-    }
+	#clock {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		width: 100vw;
+		user-select: none;
+	}
 
-    #clock #clock-digits {
-        margin: 0;
-        padding: 18% 4rem 4rem 3rem;
-        font-size: 5rem;
-        color: #fdfdfd;
-        filter: drop-shadow(0px 0px 8px rgba(0, 0, 0, .44));
-    }
+	#clock #clock-digits {
+		margin: 0;
+		padding: 18% 4rem 4rem 3rem;
+		font-size: 5rem;
+		color: #fdfdfd;
+		filter: drop-shadow(0px 0px 8px rgba(0, 0, 0, 0.44));
+	}
 </style>
