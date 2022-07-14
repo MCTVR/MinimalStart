bec4fb9a3a5642c6d90b749992c217acd1f9f2e5
MCTVR <69135058+MCTVR@users.noreply.github.com>
2022-07-14 22:02:09 +0800

    Build Dist

--- public/icons/makewebp.py
+++ public/icons/makewebp.py
@@ -1,12 +1,12 @@
 import cv2
 
-img = cv2.imread('./HK.png')
+img = cv2.imread('./HK.webp')
 
-scale = 0.7
+scale = 0.5
 width = int(img.shape[1] * scale)
 height = int(img.shape[0] * scale)
 
 dim = (width, height)
 
-cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
-cv2.imwrite("HK.webp", img, [cv2.IMWRITE_WEBP_QUALITY, 95])
+img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+cv2.imwrite("HK.1.webp", img, [cv2.IMWRITE_WEBP_QUALITY, 95])
